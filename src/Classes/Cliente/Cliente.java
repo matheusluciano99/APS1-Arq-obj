69791194e9edb929d12b558eaec3a9982dca5005
matheusluciano99 <matheusluciano99@gmail.com>
2025-08-31 20:11:47 -0300

    cliente pronto

--- src/Classes/Cliente/Cliente.java
+++ src/Classes/Cliente/Cliente.java
@@ -1,6 +1,44 @@
 package Classes.Cliente;
 
+import java.time.LocalDate;
+
 public class Cliente {
-	// TODO: 
+	private String cpf;
+	private String nome;
+	private LocalDate dataNascimento;
+	private Float salario;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Float salario) {
+        this.salario = salario;
+    }
+
 }
 
