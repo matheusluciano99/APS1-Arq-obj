31f5dea4135c49e2345a140033321c5d5a6e569d
matheusluciano99 <matheusluciano99@gmail.com>
2025-08-31 20:47:26 -0300

    cartão pronto

--- src/Classes/Cartao/Cartao.java
+++ src/Classes/Cartao/Cartao.java
@@ -1,5 +1,51 @@
 package Classes.Cartao;
 
+import java.time.LocalDate;
+
 public class Cartao {
-	// TODO: 
+	private String numeroCartao;
+	private String tipo;
+	private LocalDate validade;
+	private String status;
+
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+
+    public void setNumeroCartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+	public Boolean isExpired() {
+		return validade.isBefore(LocalDate.now());
+	}
+
+	public Boolean cancelaCartao(){
+		setStatus("CANCELADO");
+		return true;
+	}
 }
